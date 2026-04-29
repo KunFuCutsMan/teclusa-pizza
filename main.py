579b4cec09ff1bc4eba83bda864b124e10448c14
KFCMan <leonardo.a.t.m.05@gmail.com>
2026-04-29 17:15:14 -0600

    Clase TabMenu para controlar la tab de la administración del menú

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from PyQt5.QtWidgets import QApplication
-from vistas.app import Ui_Teclusa
+from vistas.app import Ui_Teclusa, TabMenu
 
 import sys
 
 class TeclusaApp(QApplication):
+    """
+    # Teclusa App
+
+    El punto de entrada de la aplicación.
+    """
 
     def __init__(self):
         super().__init__([])
-        
+
         self.ui = Ui_Teclusa(
             title="TECLUSA Pizzeria"
         )
--- vistas/app.py
+++ vistas/app.py
@@ -1,4 +1,15 @@
 from PyQt5.QtWidgets import QWidget, QVBoxLayout, QTabWidget
+from vistas.ventana_menu import Ui_VentanaMenu
+
+# NOTA:
+#
+# Las clases que se utilizan para interactuar con la aplicación son distintas
+# a las generadas por `pyuic5` mediante los archivos .ui. Las clases presentes
+# en este archivo deben heredar de `QWidget` y la la clase generada de la
+# interfaz gráfica respectiva, y exportar los eventos relacionados con ellas.
+#
+# Esto se hace para no tener problema al modificar los archivos .ui y tener que
+# exportarlos a Python.
 
 class Ui_Teclusa(QWidget):
     """
@@ -15,7 +26,22 @@ class Ui_Teclusa(QWidget):
         self.setMinimumSize(640,480)
 
         self.tabs = QTabWidget()
-
         layout = QVBoxLayout()
         layout.addWidget(self.tabs)
         self.setLayout(layout)
+
+        # Crea y vincula las tabulaciones
+        self.tabMenu = TabMenu()
+
+        self.tabs.addTab(self.tabMenu, "Menú")
+
+class TabMenu(QWidget, Ui_VentanaMenu):
+    """
+    # TabMenu
+
+    Clase que representa a la intefaz gráfica `Ui_VentanaMenu`, responsable de
+    manejar los eventos presentes en dicha interfaz.
+    """
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
